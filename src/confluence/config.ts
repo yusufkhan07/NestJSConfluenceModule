d3fb01d383caee7929d2d8f52b87e1b9e40edf3e
Yousuf Khan <yusufkhanjee@gmail.com>
2019-12-21 08:43:46 +0500

    Auth Completed

---
+++ src/confluence/config.ts
@@ -0,0 +1,7 @@
+export const CONFIG = {
+  oauth_url: 'https://auth.atlassian.com/oauth/token',
+  client_id: 'dG1t4n4ShjZ5lY33SFWHnlDhhuCwz2P5',
+  client_secret:
+    'G_Zs7mJzlzBPDXxrJS-un4uzwOINvNSoPSDDQc5vDVoOOkVYT9Q6NzzkF3K14QAS',
+  redirect_uri: `http://localhost:3000/confluence/conf-callback/`,
+};
--- src/confluence/confluence.controller.ts
+++ src/confluence/confluence.controller.ts
@@ -1,4 +1,74 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Req,
+  Query,
+  Get,
+  Redirect,
+  Res,
+} from '@nestjs/common';
+import { CONFIG } from './config';
+import Axios from 'axios';
+import { Request } from 'express';
 
 @Controller('confluence')
-export class ConfluenceController {}
+export class ConfluenceController {
+  /**
+   * A callback function which receives the Auth Grant & redirect
+   * user to /confluence/authorize Page
+   *
+   *
+   * @param  {Request} request
+   * @param  {any} res
+   * @return {void}@memberof ConfluenceController
+   */
+  @Get('/conf-callback')
+  @Redirect('/authorize?code', 302)
+  public async confCallback(@Req() request: Request, @Res() res) {
+    try {
+      res.redirect('/confluence/authorize?code=' + request.query['code']);
+    } catch (err) {}
+  }
+
+  /**
+   * Exchange Auth Grant for Auth Token
+   *
+   * @param  {any} request
+   * @param  {string} code
+   * @return {string} access_token
+   * @memberof ConfluenceController
+   */
+  @Get('/authorize')
+  public async authorize(
+    @Req() request,
+    @Query('code') code: string,
+  ): Promise<string> {
+    try {
+      const reqBody = {
+        code,
+        grant_type: 'authorization_code',
+        client_id: CONFIG.client_id,
+        client_secret: CONFIG.client_secret,
+        redirect_uri: CONFIG.redirect_uri,
+      };
+
+      const resp = await Axios.post(CONFIG.oauth_url, reqBody);
+      const token = resp.data.access_token;
+      const scope = resp.data.scope;
+      console.log('TCL: ConfluenceController -> authorize -> token', token);
+      // TODO: save token in session.
+      return token;
+    } catch (err) {
+      if (
+        err &&
+        err.response &&
+        err.response.data &&
+        err.response.data['error'] !== undefined
+      ) {
+        return err.response.data.error;
+      } else {
+        throw err;
+      }
+    }
+  }
+}
